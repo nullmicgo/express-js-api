b608131910d6442d7f1f9b19a29f37f10c1b44f5
Leman Kwok <leman@lmswork.com>
2020-02-02 19:17:37 +0800

    chore: update for firebas setup

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const app = express();
+const instance = express();
 const routes = require('./routes');
-app.use(express.json());
-app.use('/api',routes);
+instance.use(express.json());
+instance.use('/api',routes);
 
-app.use((req, res, next) => {
+instance.use((req, res, next) => {
     const err = new Error("Not Found");
     err.status = 404;
     next(err);
 });
 
-app.use((err, req, res, next) => {
+instance.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.json({
         error: {
@@ -19,8 +19,4 @@ app.use((err, req, res, next) => {
     })
 });
 
-//app.listen(3000, () => console.log('Quote API listening on port 3000!'));
-
-
-module.exports.app = app;
-//exports.default = app;
+export const app = instance;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const functions = require('firebase-functions');
-
-const { app } = require('./app');
+import { app } from './app';
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.expressRESTAPI = functions.https.onRequest(app);
+export const expressRESTAPI = functions.https.onRequest(app);
